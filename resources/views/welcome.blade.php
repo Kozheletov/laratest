917c2eb35dd77a5e2fccdd492b1901598115362a
Grigoriy Kozheletov <misteryrus@gmail.com>
2020-12-07 21:57:38 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<div class="container mx-auto">
+    <nav>
+        <ul class="flex justify-between w-1/2 mx-auto">
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Log in</a></li>
+            <li><a href="">Name</a></li>
+            <li><a href="">Log out</a></li>
+        </ul>
+    </nav>
+    <h1 class="text-center mt-4 bg-blue-200">Hello</h1>
+</div>
+</body>
+</html>
